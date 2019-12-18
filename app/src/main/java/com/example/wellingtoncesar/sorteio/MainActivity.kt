11ae011581f1a7fe18e678945e47329c46ca2ca0
Wellington <wellingtoncesarg@gmail.com>
2019-12-18 20:36:20 -0300

    start commit

---
+++ app/src/main/java/com/example/wellingtoncesar/sorteio/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.wellingtoncesar.sorteio
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sortearNumero(view: View){
+        var texto = findViewById<TextView>(R.id.textoSorteio)
+
+        var numeroSorteado = Random().nextInt(11)
+        texto.setText("Número sorteado é: $numeroSorteado ")
+
+    }
+}
